f8ecd81f7ca6b0526785823c46314173e4296459
MoAlhaddad <mohamadalhaddad25@gmail.com>
2024-08-16 00:27:22 -0400

    attempting

---
+++ server/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+        {name: "inventory-management",
+         script: "npm",
+         args: "run dev",
+         env: {
+                NODE_ENV: "development",
+                ENV_VAR1: "environment-variable",
+         }
+        }
+    ]
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -31,7 +31,7 @@ app.use("/expenses", expenseRoutes)
 /* To test route use curl routepath in terminal
 /*SERVER */
 
-const port = process.env.PORT || 3001
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3001
+app.listen(port, "0.0.0.0",() => {
     console.log(`Server running on port ${port}`)
 })
